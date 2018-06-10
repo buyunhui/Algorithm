ac8c3a7202068a92be97c7d99fe1a9b5bf8e5717
buyunhui <suiyelian@163.com>
2018-06-10 20:52:22 +0800

    tets

---
+++ DesignPattern/cpp/decorator/common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMMON_h
+#define __COMMMON_h
+
+#include <iostream>
+#include <vector>
+#inc <stri按时ng>
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::vectorasd;
+
+using std::string;
+
+#endif
+
